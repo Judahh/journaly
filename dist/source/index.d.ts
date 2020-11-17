44767e24eedce001ea8d8751b74abf155b7bcbe2
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2020-11-17 14:51:02 -0300

    update

--- dist/source/index.d.ts
+++ dist/source/index.d.ts
@@ -1,4 +1,8 @@
 import { Journaly } from './journaly/journaly';
+import { PublisherSubscriber } from './journaly/publisherSubscriber';
+import { PublisherSubscriberWithMemory } from './journaly/publisherSubscriberWithMemory';
+import { SubjectObserver } from './journaly/subjectObserver';
+import { SubjectObserverWithMemory } from './journaly/subjectObserverWithMemory';
 import { Observer } from './observer/observer';
-export { Journaly, Observer };
+export { Journaly, Observer, PublisherSubscriber, PublisherSubscriberWithMemory, SubjectObserver, SubjectObserverWithMemory, };
 //# sourceMappingURL=index.d.ts.map
--- dist/source/journaly/journaly.d.ts
+++ dist/source/journaly/journaly.d.ts
@@ -1,11 +1,12 @@
-import { Observer } from '../observer/observer';
-export declare class Journaly<Result> implements Observer {
-    private subscribers;
-    private oldData;
-    constructor(hasMemory?: any);
-    subscribe(subject: string, subscriber: (...params: any[]) => Promise<Result>): Promise<Array<Result>>;
-    unsubscribe(subject: string, subscriber: (...params: any[]) => Promise<Result>): Array<(...params: any[]) => Promise<Result>>;
-    publish(subject: string, ...params: any[]): Promise<Array<Result>>;
-    private checkSubscribers;
+import { PublisherSubscriber } from './publisherSubscriber';
+import { PublisherSubscriberWithMemory } from './publisherSubscriberWithMemory';
+import { SubjectObserver } from './subjectObserver';
+import { SubjectObserverWithMemory } from './subjectObserverWithMemory';
+export declare class Journaly {
+    private constructor();
+    static newJournaly<Result>(options?: {
+        hasMemory?: boolean;
+        multiple?: boolean;
+    }): PublisherSubscriberWithMemory<Result> | PublisherSubscriber<Result> | SubjectObserverWithMemory<Result> | SubjectObserver<Result>;
 }
 //# sourceMappingURL=journaly.d.ts.map
